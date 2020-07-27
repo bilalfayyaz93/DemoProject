db9005375db007718a7b9d3b2a21dc28f267a970
bilal ahmad <bilalfayyaz93@gmail.com>
2020-07-27 23:19:09 +0500

    image upload issue resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :display_picture
-<<<<<<< HEAD
-=======
+  has_one_attached :avator
   has_many :comments
   has_many :orders
   has_many :products
   has_one :cart
->>>>>>> 38e3601b1fb887ff3a37553593f13914a8cdb188
 end
