087cd607d00c57fb86d72451a9ea5e72ce63273c
Bilal ahmad <41960755+bilalfayyaz93@users.noreply.github.com>
2020-07-28 16:38:44 +0500

    remove photo functionality added (#4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_accessor :remove_avator
+
+  after_save :purge_avator, if: :remove_avator
+  def purge_avator
+    self.avator.purge_later
+  end
   has_one_attached :avator
   has_many :comments
   has_many :orders
   has_many :products
   has_one :cart
+
+
 end
