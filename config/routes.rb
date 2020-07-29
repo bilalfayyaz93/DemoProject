688cea028fe1fc6ed56a266ea80f9f5d2aa00462
bilal ahmad <bilalfayyaz93@gmail.com>
2020-07-29 14:42:51 +0500

    product images added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  root 'home#index'
+  root 'products#index'
   devise_for :users
+  resources :products do
+    member do
+      delete :delete_image
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
