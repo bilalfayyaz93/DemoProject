b843a693451a01e0400e58c00dad1abbd025f1f7
Bilal ahmad <41960755+bilalfayyaz93@users.noreply.github.com>
2020-07-30 16:06:45 +0500

    comments added (#7)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
+    @comments= @product.comments.all
   end
 
   # GET /products/new
