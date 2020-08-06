697b10d2b1361009292a9260cf90fc31c8b9de04
Bilal ahmad <41960755+bilalfayyaz93@users.noreply.github.com>
2020-08-07 03:47:37 +0500

    stripe implemented (#9)

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,8 @@
 Stripe.api_key = "sk_test_51HD8DJBKQ0RylwgbcHr0sfcCVohDHVfOwawstomctY90zYfhq69eDbhJDctKZYEHrswwvwZ7y10BRxN7jav6Jhx500gwIcqqLN"
 STRIPE_PUBLIC_KEY = "pk_test_51HD8DJBKQ0RylwgbRtRqQqFB4yzpxvdyBaAe2IDxUGMklfSdt6w0OCfTJufrH0UormFZRuZFKoYdBRV4bFyRyJfy00ZsVfuL3R"
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
