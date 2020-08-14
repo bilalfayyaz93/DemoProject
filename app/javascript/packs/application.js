d97a9046c1427e97a84942d2476c5d3061f1efa1
bilal ahmad <bilalfayyaz93@gmail.com>
2020-08-14 16:17:32 +0500

    bootstrap 4 added using yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 //= require order
 //= require jquery
 //= require jquery3
